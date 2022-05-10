329f54415a882c1790191384a76179b2358d2680
Alexey Streltsov <105157892+Ym6ra@users.noreply.github.com>
2022-05-10 14:57:55 +0300

    Update worker_update.php

--- app/worker_update.php
+++ app/worker_update.php
@@ -5,33 +5,33 @@
 <BODY>
     <?php
         if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $url = ('https://online.moysklad.ru/api/remap/1.2/entity/employee'); 
-        $login = base64_decode($_POST['login']);
-        $password = base64_decode($_POST['password']);
-        $headers = array(
-            'Content-Type:application/json',
-            'Authorization: Basic '.base64_encode("$login:$password")
-            );
-        $ch = curl_init();
-        $id = $_POST['id'];
-        $n = $_POST['n'];
-        $data_worker = array(
-            "firstName" => (string)$_POST['firstName'],
-            "middleName" => (string)$_POST['middleName'],
-            "lastName" => (string)$_POST['lastName'],
-            "email" => (string)$_POST['email'],
-            );
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data_worker, JSON_UNESCAPED_UNICODE)); 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        curl_setopt($ch, CURLOPT_URL, $url.'/'.$id);
-        $response = curl_exec($ch);
-        $data = json_decode($response, 1);
-        curl_close($ch);
+            $url = ('https://online.moysklad.ru/api/remap/1.2/entity/employee'); 
+            $login = base64_decode($_POST['login']);
+            $password = base64_decode($_POST['password']);
+            $headers = array(
+                'Content-Type:application/json',
+                'Authorization: Basic '.base64_encode("$login:$password")
+                );
+            $ch = curl_init();
+            $id = $_POST['id'];
+            $n = $_POST['n'];
+            $data_worker = array(
+                "firstName" => (string)$_POST['firstName'],
+                "middleName" => (string)$_POST['middleName'],
+                "lastName" => (string)$_POST['lastName'],
+                "email" => (string)$_POST['email'],
+                );
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data_worker, JSON_UNESCAPED_UNICODE)); 
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_URL, $url.'/'.$id);
+            $response = curl_exec($ch);
+            $data = json_decode($response, 1);
+            curl_close($ch);
         }
-        ?>
+    ?>
     <div>
         <p>Id Работника: <?php echo ($data["id"]);?></p>
         <p>Доступная информация:<br></p>
